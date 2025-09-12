4909fd2da23075d156b3dcaf8b267261f04ff357
louisaldsr <aldasorolouis@gmail.com>
2025-09-13 01:17:31 +0200

    without public

--- src/helpers/loaders/csv-reader.ts
+++ src/helpers/loaders/csv-reader.ts
@@ -5,13 +5,7 @@ export default async function readCsv(
   competition: string,
   fileName: string
 ): Promise<string[]> {
-  const csvPath = path.join(
-    process.cwd(),
-    "public",
-    "data",
-    competition,
-    fileName
-  );
+  const csvPath = path.join(process.cwd(), "data", competition, fileName);
   const rawFileContent = await fs.readFile(csvPath, "utf-8");
   return rawFileContent.split("\n");
 }
