ad36c74a86bf8ce01fbc7331a8e370b991ff8b35
louisaldsr <aldasorolouis@gmail.com>
2025-09-13 14:47:20 +0200

    try something

--- src/helpers/loaders/csv-reader.ts
+++ src/helpers/loaders/csv-reader.ts
@@ -1,5 +1,4 @@
 import { headers } from "next/headers";
-import path from "path";
 
 export default async function readCsv(
   competition: string,
@@ -7,17 +6,16 @@ export default async function readCsv(
 ): Promise<string[]> {
   const host =
     (await headers()).get("x-forwarded-host") || (await headers()).get("host");
-  const base = !host?.includes("localhost")
-    ? `https://${host}`
-    : "http://localhost:3000";
-  const csvPath = path.join(base, "data", competition, fileName);
+  const csvUrl = new URL(`/data/${competition}/${fileName}`, `http://${host}`);
 
   try {
-    const res = await fetch(csvPath, { cache: "force-cache" });
+    const res = await fetch(csvUrl, { cache: "force-cache" });
     if (!res.ok) throw new Error(res.statusText);
     const rawFileContent = await res.text();
     return rawFileContent.split("\n");
   } catch (error) {
-    throw new Error(`Failed to fetch [csvPath: ${csvPath}]: ${error}`);
+    throw new Error(
+      `Failed to fetch [csvPath: ${csvUrl.toString()}]: ${error}`
+    );
   }
 }
