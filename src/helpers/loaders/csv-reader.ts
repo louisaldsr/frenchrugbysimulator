e1da72634e4b9f296ced73dca441da52c1398f06
louisaldsr <aldasorolouis@gmail.com>
2025-09-13 14:36:17 +0200

    try convert fs read by fetch

--- src/helpers/loaders/csv-reader.ts
+++ src/helpers/loaders/csv-reader.ts
@@ -1,11 +1,23 @@
-import { promises as fs } from "fs";
+import { headers } from "next/headers";
 import path from "path";
 
 export default async function readCsv(
   competition: string,
   fileName: string
 ): Promise<string[]> {
-  const csvPath = path.join(process.cwd(), "data", competition, fileName);
-  const rawFileContent = await fs.readFile(csvPath, "utf-8");
-  return rawFileContent.split("\n");
+  const host =
+    (await headers()).get("x-forwarded-host") || (await headers()).get("host");
+  const base = !host?.includes("localhost")
+    ? `https://${host}`
+    : "http://localhost:3000";
+  const csvPath = path.join(base, "data", competition, fileName);
+
+  try {
+    const res = await fetch(csvPath, { cache: "force-cache" });
+    if (!res.ok) throw new Error(res.statusText);
+    const rawFileContent = await res.text();
+    return rawFileContent.split("\n");
+  } catch (error) {
+    throw new Error(`Failed to fetch [csvPath: ${csvPath}]: ${error}`);
+  }
 }
